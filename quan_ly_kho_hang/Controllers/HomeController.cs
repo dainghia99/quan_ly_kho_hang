5a6abc9f5fe9f015f60e87096b07e364c956f6e9
dainghia99 <dainghiakh@gmail.com>
2025-09-24 20:26:33 +0700

    Add project files.

---
+++ quan_ly_kho_hang/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using quan_ly_kho_hang.Models;
+
+namespace quan_ly_kho_hang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ quan_ly_kho_hang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quan_ly_kho_hang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
