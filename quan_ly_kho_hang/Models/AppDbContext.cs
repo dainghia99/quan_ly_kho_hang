67faf07326690f15f437538108d448ae51dff0f3
dainghia99 <dainghiakh@gmail.com>
2025-09-24 21:02:58 +0700

    Thêm Identity vào ứng dụng

---
+++ quan_ly_kho_hang/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace quan_ly_kho_hang.Models
+{
+    public class AppDbContext : DbContext
+    {
+        
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+
+        }
+
+
+    }
+}
--- quan_ly_kho_hang/Program.cs
+++ quan_ly_kho_hang/Program.cs
@@ -1,7 +1,82 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using quan_ly_kho_hang.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Đọc cấu hình từ appsettings.json
+var mongoConnection = builder.Configuration.GetConnectionString("MongoDb");
+var mongoDatabase = builder.Configuration.GetConnectionString("DatabaseName");
+
+// Đăng ký MongoClient và DbContext
+builder.Services.AddSingleton<MongoClient>(sp => new MongoClient(mongoConnection));
+builder.Services.AddDbContext<AppDbContext>((sp, options) =>
+{
+    var client = sp.GetRequiredService<MongoClient>();
+    options.UseMongoDB(client, mongoDatabase);
+});
+
+
+// Truy cập IdentityOptions
+builder.Services.Configure<IdentityOptions>(options => {
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lầ thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/login/";
+    options.LogoutPath = "/logout/";
+    options.AccessDeniedPath = "/khongduoctruycap.html";
+});
+
+builder.Services.AddAuthentication()
+        .AddGoogle(options => {
+            var gconfig = builder.Configuration.GetSection("Authentication:Google");
+            options.ClientId = gconfig["ClientId"];
+            options.ClientSecret = gconfig["ClientSecret"];
+            // https://localhost:5001/signin-google
+            options.CallbackPath = "/dang-nhap-tu-google";
+        })
+        .AddFacebook(options => {
+            var fconfig = builder.Configuration.GetSection("Authentication:Facebook");
+            options.AppId = fconfig["AppId"];
+            options.AppSecret = fconfig["AppSecret"];
+            options.CallbackPath = "/dang-nhap-tu-facebook";
+        })
+        // .AddTwitter()
+        // .AddMicrosoftAccount()
+        ;
+
+//builder.Services.AddSingleton<IdentityErrorDescriber, AppIdentityErrorDescriber>();
+
+
 
 var app = builder.Build();
 
@@ -18,6 +93,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
